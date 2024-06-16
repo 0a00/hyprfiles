9595f4a5f85baeccb6b14e216621a317db0cb5ac
0a00 <2983531459@qq.com>
2024-06-16 21:42:31 +0800

    update

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -2,22 +2,25 @@
 -- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
 -- Forum: https://www.reddit.com/r/lunarvim/
 -- Discord: https://discord.com/invite/Xb9B4Ny
+--
+lvim.format_on_save.enabled = true
 lvim.transparent_window = true
-lvim.colorscheme = "default"
+lvim.colorscheme = "NeoSolarized"
 lvim.builtin.lualine.style = "lvim" -- or "none"
 
 lvim.autocommands = {
-    {
-        "BufEnter", -- see `:h autocmd-events`
-        { -- this table is passed verbatim as `opts` to `nvim_create_autocmd`
-            command = "highlight CursorLine   cterm=NONE ctermbg=NONE ctermfg=NONE guibg=NONE guifg=NONE",
-        }
-    },
+  {
+    "BufEnter", -- see `:h autocmd-events`
+    {           -- this table is passed verbatim as `opts` to `nvim_create_autocmd`
+      command = "highlight CursorLine   cterm=NONE ctermbg=NONE ctermfg=NONE guibg=NONE guifg=NONE",
+    }
+  },
 }
 
 
--- plugins 
+-- plugins
 lvim.plugins = {
+  { "Tsuzat/NeoSolarized.nvim" },
 }
 
 local colors = {
